2aad54b3d7512f76ccb2aeb90095581df6b0d510
Gerhard Schoosleitner <gasch@GertschMary.fritz.box>
2024-08-08 22:23:14 +0200

    Fixed odd style - Month Calendar Style is broken in W11!

--- BillEdit.dpr
+++ BillEdit.dpr
@@ -5,13 +5,16 @@ uses
   uniFrmMain in 'uniFrmMain.pas' {BillEditor},
   uniFrmManualChange in 'uniFrmManualChange.pas' {DialogChange},
   uniSettingsMgr in 'uniSettingsMgr.pas',
-  uniLangManager in 'uniLangManager.pas';
+  uniLangManager in 'uniLangManager.pas',
+  Vcl.Themes,
+  Vcl.Styles;
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Windows10');
   Application.CreateForm(TBillEditor, BillEditor);
   Application.CreateForm(TDialogChange, DialogChange);
   Application.Run;
